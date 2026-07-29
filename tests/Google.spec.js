46f92df07122e2b030ba5cf1d90ef40a94cfb5c8
Suryasai693 <suryasai693@gmail.com>
2026-07-29 13:54:45 +0530

    Initial Playwright CI Project

---
+++ tests/Google.spec.js
@@ -0,0 +1,12 @@
+const {test, expect} = require('@playwright/test');
+
+const GooglePage = require('../node_modules/pages/GooglePage.spec');
+
+test ('Verify Google Title', async ({page}) =>{
+
+  const google = new GooglePage(page);
+
+  await google.openGoogle();
+
+  await expect(page).toHaveTitle('Google');
+});
